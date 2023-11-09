361c4d3482ad1cbd7188abcdf9452368596a764b
Uladzislau <u.yanusheuski@andersenlab.com>
2023-11-09 09:46:22 +0100

    feat : added changes for the exist app

--- src/components/pages/PageProducts/PageProducts.tsx
+++ src/components/pages/PageProducts/PageProducts.tsx
@@ -1,10 +1,16 @@
+/** @format */
+
 import Products from "~/components/pages/PageProducts/components/Products";
 import Box from "@mui/material/Box";
+import TaskImage from "assets/images/module_focus.png";
 
 export default function PageProducts() {
-  return (
-    <Box py={3}>
-      <Products />
-    </Box>
-  );
+	return (
+		<Box py={3}>
+			Task_2_
+			<br />
+			<img src={TaskImage} alt="TaskImage" height={300} />
+			<Products />
+		</Box>
+	);
 }
